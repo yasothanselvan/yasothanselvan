742452dc5509c55305eeb474697ee7dca2ac1bba
Panda <medicinememeclub@gmail.com>
2025-06-28 02:38:04 +0800

    change the environment

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ class App{
         
         const self = this;
         
-        loader.load( './assets/hdr/venice_sunset_1k.hdr', ( texture ) => {
+        loader.load( './assets/hdr/Cobblestone Street Night.hdr', ( texture ) => {
           const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
           pmremGenerator.dispose();
 
